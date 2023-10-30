0eace3b610881b6b8bad9d25c956ce269bd43859
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-10-30 17:14:04 +0000

    working rowmajor 90 and 180 CW

--- locality/ppmtrans/src/main.rs
+++ locality/ppmtrans/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
 
     let init_img = Array2::new_array(img.pixels.clone(), img.width as usize, img.height as usize);
 
-    let rotated_img = rotate_90(&init_img);
+    let rotated_img = rotate_180(&init_img);
 
     let rotated_image = RgbImage {
         width: rotated_img.width as u32,
@@ -27,7 +27,23 @@ fn main() {
     rotated_image.write(Some("output.ppm")).unwrap();
 }
 
-fn rotate_90(input_image: &Array2<Rgb>) -> Array2<Rgb> {
+fn rotate_colmajor_90(input_image: &Array2<Rgb>) -> Array2<Rgb> {
+    let mut rotated_data = Vec::new();
+
+    for (row, col, pixel) in input_image.iter_col_major(){
+        let pixel = input_image.get_element(input_image.height - row - 1, col);
+        rotated_data.push(Rgb {
+            red: pixel.red,
+            green: pixel.green,
+            blue: pixel.blue,
+        });
+    }
+
+    Array2::new_array(rotated_data, input_image.height, input_image.width)
+}
+
+
+fn rotate_rowmajor_90(input_image: &Array2<Rgb>) -> Array2<Rgb> {
     let mut rotated_data = Vec::new();
 
     for (row, col, pixel) in input_image.iter_row_major(){
@@ -40,4 +56,9 @@ fn rotate_90(input_image: &Array2<Rgb>) -> Array2<Rgb> {
     }
 
     Array2::new_array(rotated_data, input_image.height, input_image.width)
+}
+
+fn rotate_rowmajor_180(input_image: &Array2<Rgb>) -> Array2<Rgb> {
+    let first_rotation = rotate_90(input_image);
+    rotate_90(&first_rotation)
 }

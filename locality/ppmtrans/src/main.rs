a992b301e1fcccc798c9395dffaa6bc3a3cebd5c
samuelcalise <113198869+samuelcalise@users.noreply.github.com>
2023-10-30 16:56:12 +0000

    working iterator with row_major with rotate 90CW

--- locality/ppmtrans/src/main.rs
+++ locality/ppmtrans/src/main.rs
@@ -1,9 +1,9 @@
-use csc411_image::{Read};
+use csc411_image::Read;
 use csc411_image::RgbImage;
-use std::io;
-use array2::Array2;
 use csc411_image::Write;
 use csc411_image::Rgb;
+use std::io;
+use array2::Array2;
 
 fn main() {
     let mut input = String::new();
@@ -13,32 +13,30 @@ fn main() {
 
     let img = RgbImage::read(Some(trimmed_input)).unwrap();
 
-    let init_img = Array2::new_array(img.pixels.clone(), img.width as usize, img.height as usize); // Clone the pixel data
+    let init_img = Array2::new_array(img.pixels.clone(), img.width as usize, img.height as usize);
 
     let rotated_img = rotate_90(&init_img);
 
     let rotated_image = RgbImage {
-        width: img.height,
-        height: img.width,
+        width: rotated_img.width as u32,
+        height: rotated_img.height as u32,
         denominator: img.denominator,
         pixels: rotated_img.vec_of_val,
     };
 
-    rotated_image.write(Some("output.ppm"));
+    rotated_image.write(Some("output.ppm")).unwrap();
 }
 
 fn rotate_90(input_image: &Array2<Rgb>) -> Array2<Rgb> {
     let mut rotated_data = Vec::new();
 
-    for col in 0..input_image.width {
-        for row in (0..input_image.height).rev() {
-            let pixel = input_image.get_element(row, col);
-            rotated_data.push(Rgb {
-                red: pixel.red,
-                green: pixel.green,
-                blue: pixel.blue,
-            });
-        }
+    for (row, col, pixel) in input_image.iter_row_major(){
+        let pixel = input_image.get_element(input_image.height - row - 1, col);
+        rotated_data.push(Rgb {
+            red: pixel.red,
+            green: pixel.green,
+            blue: pixel.blue,
+        });
     }
 
     Array2::new_array(rotated_data, input_image.height, input_image.width)
